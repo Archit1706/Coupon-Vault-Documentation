00b805896e0fb39564267c83e106257a67cd0dfb
Archit <architrathod77@gmail.com>
2023-06-26 11:59:40 +0530

    Minor Changes

--- theme.config.jsx
+++ theme.config.jsx
@@ -57,11 +57,10 @@ export default {
     footer: {
         text: (
             <span>
-                MI {new Date().getFullYear()} ©{" "}
                 <a href="https://coupon-vault.vercel.app" target="_blank">
                     Coupon Vault
                 </a>
-                .
+                {new Date().getFullYear()} ©{" "}
             </span>
         ),
     },
